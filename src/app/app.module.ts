ec4bd3a18ffbf07e31221d3e375bc9c74e885521
sasankdas <alladisasankdas@gmail.com>
2018-03-15 22:28:01 -0400

    login component - through Reactive Approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
@@ -33,6 +34,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     AppRoutingModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
