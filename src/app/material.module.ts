c66970d50698b90326acc9370cc5321a8de25733
sasankdas <alladisasankdas@gmail.com>
2018-03-16 14:30:59 -0400

    added side navigation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
-MatDatepickerModule, MatNativeDateModule,MatCheckboxModule } from '@angular/material';
+MatDatepickerModule, MatNativeDateModule,MatCheckboxModule, MatSidenavModule } from '@angular/material';
 
 
 @NgModule({
     imports:[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule,
-        MatCheckboxModule, MatNativeDateModule],
+        MatCheckboxModule, MatNativeDateModule,MatSidenavModule],
     exports :[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule,
-        MatCheckboxModule, MatNativeDateModule],
+        MatCheckboxModule, MatNativeDateModule, MatSidenavModule],
 })
 export class MaterialModule{
 
