a77f333e7bcd527ad7979f635fc961a1a85a6cf3
sasankdas <alladisasankdas@gmail.com>
2018-03-16 18:21:48 -0400

    Side nav and Toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
-MatDatepickerModule, MatNativeDateModule,MatCheckboxModule, MatSidenavModule } from '@angular/material';
+MatDatepickerModule, MatNativeDateModule,MatCheckboxModule, MatSidenavModule
+,MatToolbarModule } from '@angular/material';
 
 
 @NgModule({
     imports:[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule,
-        MatCheckboxModule, MatNativeDateModule,MatSidenavModule],
+        MatCheckboxModule, MatNativeDateModule,MatSidenavModule, MatToolbarModule],
     exports :[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule,
-        MatCheckboxModule, MatNativeDateModule, MatSidenavModule],
+        MatCheckboxModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule],
 })
 export class MaterialModule{
 
