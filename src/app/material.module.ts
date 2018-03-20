d0cac2fdb803cba7529245359b8f8bf9b4e3f6b3
sasankdas <alladisasankdas@gmail.com>
2018-03-19 20:20:59 -0400

    Added spinner to Training Component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from "@angular/core";
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
 MatDatepickerModule, MatNativeDateModule,MatCheckboxModule, MatSidenavModule
-,MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule } from '@angular/material';
+,MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+MatProgressSpinnerModule } from '@angular/material';
 
 
 @NgModule({
     imports:[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule,
         MatCheckboxModule, MatNativeDateModule,MatSidenavModule, MatToolbarModule,
-         MatListModule, MatTabsModule, MatCardModule, MatSelectModule],
+         MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule],
     exports :[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule,
         MatCheckboxModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule,
-         MatListModule, MatTabsModule, MatCardModule, MatSelectModule],
+         MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule],
 })
 export class MaterialModule{
 
